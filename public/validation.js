12a234cc34e64b828c5f3aac01f5c391c70a438b
tarun-gk <tarun.gadadasu@gmail.com>
2026-02-26 21:08:49 +0530

    fbs ecommerce project

---
+++ public/validation.js
@@ -0,0 +1,1 @@
+// validation file (Week-2)
---
+++ server.js
@@ -0,0 +1,26 @@
+// server.js
+constexpress = require('express');
+constaxios = require('axios');
+
+constapp = express();
+constport = 3000;
+
+app.use(express.static('public'));
+
+app.get('/weather/:city', async (req, res) => {
+  const{ city } = req.params;
+
+  try {
+    constapiKey = 'c97c0c1086d42990e89d64d76f50bb61';
+    constresponse = awaitaxios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+    consttemperature = response.data.main.temp;
+    res.json({ temperature });
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    res.status(500).json({ error:'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
